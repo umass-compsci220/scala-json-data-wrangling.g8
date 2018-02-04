ab8dbb86207233bf9ee720ba497c1ad76bfd7e3c
Rachit Nigam <rachit.nigam12@gmail.com>
2018-02-04 09:36:59 -0500

    fill out template

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,30 @@
-object Wrangling {
+import hw.json._
+import hw.wrangling.WranglingLike
 
+object Wrangling extends WranglingLike {
+
+  val data: List[Json] = Json.fromFile("yelp.json")
+
+  def key(json: Json, key: String): Option[Json] = ???
+
+  def fromState(data: List[Json], state: String): List[Json] = ???
+
+  def ratingLT(data: List[Json], rating: Int): List[Json] = ???
+
+  def ratingGT(data: List[Json], rating: Int): List[Json] = ???
+
+  def category(data: List[Json], category: String): List[Json] = ???
+
+  def groupByState(data: List[Json]): Map[String, List[Json]] = ???
+
+  def groupByCategory(data: List[Json]): Map[String, List[Json]] = ???
+
+  def bestPlace(data: List[Json]): Json = ???
+
+  def hasAmbience(data: List[Json], ambience: String): List[Json] = ???
+
+  def main(args: Array[String]) = {
+    println(data.head)
+  }
 
 }
